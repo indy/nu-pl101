b1e9836afbceb551134711657eeff08a25a9a75a
Inderjit Gill <email@indy.io>
2012-04-16 23:44:30 +0100

    implemented function that compiles MUS into NOTE

--- mus/compiler.js
+++ mus/compiler.js
@@ -1,8 +1,58 @@
-// endTime, compileT, and compile.
+var reverse = function(expr) {
+    if(expr.tag === 'note') {
+        return expr;
+    } else {
+        return {tag: 'seq',
+                left: reverse(expr.right),
+                right: reverse(expr.left)
+               };
+    }
+};
 
+var endTime = function (time, expr) {
+  var duration = function(exp) {
+    if(exp.tag === 'note') {
+      return exp.dur;
+    } else if(exp.tag === 'seq') {
+      return duration(exp.left) + duration(exp.right);
+    } else if(exp.tag === 'par') {
+      var lDur = duration(exp.left);
+      var rDur = duration(exp.right);
+      return lDur > rDur ? lDur : rDur;
+    } else {
+      throw "endTime error";
+    }
+  };
+  return duration(expr) + time;
+};
 
+function traverse(res, time, expr) {
+  if(expr.tag === 'note') {
+    res.push({ tag: expr.tag, 
+               pitch: expr.pitch, 
+               dur: expr.dur, 
+               start: time});
+  } else if(expr.tag === 'seq') {
+    res = traverse(res, time, expr.left);
+    res = traverse(res, endTime(time, expr.left), expr.right);
+  } else if(expr.tag === 'par'){
+    res = traverse(res, time, expr.left);
+    res = traverse(res, time, expr.right);
+  } else {
+    throw "error";
+  }
+  return res;
+}
 
+var compile = function (musexpr) {
+  return traverse([], 0, musexpr);
+};
 
+/*
+var playMUS = function(musexpr) {
+  playNote(compile(musexpr));
+};
+*/
 
 var melody_mus = 
     { tag: 'seq',
@@ -16,6 +66,4 @@ var melody_mus =
          right: { tag: 'note', pitch: 'd4', dur: 500 } } };
 
 console.log(melody_mus);
-//console.log(compile(melody_mus));
-
-
+console.log(compile(melody_mus));
