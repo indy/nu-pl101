3690204ad45803478c2675bf1fc8896ea1f58ec4
Inderjit Gill <email@indy.io>
2012-04-17 00:07:55 +0100

    NOTE output includes MIDI numbers

--- mus/compiler.js
+++ mus/compiler.js
@@ -1,3 +1,18 @@
+var midiNumbers = { "a0": 22, "b0": 23, "c1": 24, "d1": 26,
+                    "e1": 28, "f1": 29, "g1": 31, "a1": 33,
+                    "b1": 35, "c2": 36, "d2": 38, "e2": 40,
+                    "f2": 41, "g2": 43, "a2": 45, "b2": 47,
+                    "c3": 48, "d3": 50, "e3": 52, "f3": 53,
+                    "g3": 55, "a3": 57, "b3": 59, "c4": 60,
+                    "d4": 62, "e4": 64, "f4": 65, "g4": 67,
+                    "a4": 69, "b4": 71, "c5": 72, "d5": 74,
+                    "e5": 76, "f5": 77, "g5": 79, "a5": 81,
+                    "b5": 83, "c6": 84, "d6": 86, "e6": 88,
+                    "f6": 89, "g6": 91, "a6": 93, "b6": 95,
+                    "c7": 96, "d7": 98, "e7": 100, "f7": 101,
+                    "g7": 103, "a7": 105, "b7": 107, "c8": 108
+                  };
+
 var reverse = function(expr) {
     if(expr.tag === 'note') {
         return expr;
@@ -31,7 +46,7 @@ var endTime = function (time, expr) {
 function traverse(res, time, expr) {
   if(expr.tag === 'note') {
     res.push({ tag: expr.tag, 
-               pitch: expr.pitch, 
+               pitch: midiNumbers[expr.pitch], 
                dur: expr.dur, 
                start: time });
   } else if(expr.tag === 'seq') {
