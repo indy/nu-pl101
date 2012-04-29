8c7dc53111ff2e289a84b109208c3bd087cffac1
Inderjit Gill <email@indy.io>
2012-04-29 18:31:12 +0100

    began implementation of scheem interpreter

---
+++ scheem/test/compiler.test.js
@@ -0,0 +1,13 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+// var compiler = require('../lib/compiler');
+
+
+describe("Compiler", function(){
+
+  it("should use expect from chai", function() {
+    expect(1).to.equal(1);
+  });
+
+});
