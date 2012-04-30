707bc7e1eee3ba2f4abb195bab21230ab301957d
Inderjit Gill <email@indy.io>
2012-04-30 10:18:54 +0100

    variables are being tested

--- scheem/lib/interpreter.js
+++ scheem/lib/interpreter.js
@@ -1,6 +1,5 @@
 
-
-exports.evalScheem = function (expr) {
+var evl = function (expr, env) {
   var i, xs, x, res, op;
 
   // Numbers evaluate to themselves
@@ -16,59 +15,62 @@ exports.evalScheem = function (expr) {
   // Look at head of list for operation
   switch (expr[0]) {
   case '+':
-    return evalScheem(expr[1]) + evalScheem(expr[2]);
+    return evl(expr[1], env) + evl(expr[2], env);
   case '-':
-    return evalScheem(expr[1]) - evalScheem(expr[2]);
+    return evl(expr[1], env) - evl(expr[2], env);
   case '*':
-    return evalScheem(expr[1]) * evalScheem(expr[2]);
+    return evl(expr[1], env) * evl(expr[2], env);
   case '/':
-    return evalScheem(expr[1]) / evalScheem(expr[2]);
+    return evl(expr[1], env) / evl(expr[2], env);
 
   case 'define':
-    env[expr[1]] = evalScheem(expr[2], env);
+    env[expr[1]] = evl(expr[2], env);
     return 0;
   case 'set!':
-    env[expr[1]] = evalScheem(expr[2], env);
+    env[expr[1]] = evl(expr[2], env);
     return 0;
   case 'begin':
     for(i=1;i<expr.length;i+=1) {
-      res = evalScheem(expr[i], env);
+      res = evl(expr[i], env);
     }
     return res;
   case 'quote':
     return expr[1];
 
   case '=':
-    op = (evalScheem(expr[1], env) === evalScheem(expr[2], env));
+    op = (evl(expr[1], env) === evl(expr[2], env));
     return op ? '#t' : '#f';
   case '<':
-    op = (evalScheem(expr[1], env) < evalScheem(expr[2], env));
+    op = (evl(expr[1], env) < evl(expr[2], env));
     return op ? '#t' : '#f';
   case '>':
-    op = (evalScheem(expr[1], env) > evalScheem(expr[2], env));
+    op = (evl(expr[1], env) > evl(expr[2], env));
     return op ? '#t' : '#f';
 
   case 'cons':
-    x = evalScheem(expr[1], env);
-    xs = evalScheem(expr[2], env);
+    x = evl(expr[1], env);
+    xs = evl(expr[2], env);
     xs.unshift(x);
     return xs;
   case 'car':
-    xs = evalScheem(expr[1], env);
+    xs = evl(expr[1], env);
     return xs[0];
   case 'cdr':
-    xs = evalScheem(expr[1], env);
+    xs = evl(expr[1], env);
     xs.splice(0, 1);
     return xs;
 
   case 'if':
-    res = evalScheem(expr[1], env);
+    res = evl(expr[1], env);
     if(res === '#t') {
-      return evalScheem(expr[2], env);
+      return evl(expr[2], env);
     } else {
-      return evalScheem(expr[3], env);
+      return evl(expr[3], env);
     }
 
   }
 
 };
+
+
+module.exports.evalScheem = evl;
--- scheem/test/interpreter.test.js
+++ scheem/test/interpreter.test.js
@@ -2,27 +2,80 @@ var interpreter = require('../lib/interpreter');
 var chai = require('chai');
 var assert = chai.assert;
 
-function testEval(expression, env, expected) {
-  assert.deepEqual(interpreter.evalScheem(expression, env), 
-                   expected);
+function evalShould(desc, expression, env, expected) {
+  it(desc, function() {
+    assert.deepEqual(interpreter.evalScheem(expression, env), 
+                     expected);
+  });
 }
+
+describe('arithmetic', function() {
+
+  evalShould('return a number',
+             5, {},
+             5);
+
+  evalShould('add two numbers',
+             ['+', 2, 3], {},
+             5);
+
+  evalShould('multiply two numbers',
+             ['*', 2, 3], {},
+             6);
+
+  evalShould('subtract two numbers',
+             ['-', 5, 3], {},
+             2);
+
+  evalShould('divide two numbers',
+             ['/', 10, 2], {},
+             5);
+
+  evalShould('combined arithmetic operations',
+             ['*', ['/', 8, 4], ['+', 1, 1]], {},
+             4);
+
+});
+
+describe('variables', function() {
+  
+  var env = {x:2, y:3, z:10};
+
+  evalShould('return a number',
+             5, env,
+             5);
+
+  evalShould('lookup a variable',
+             'x', env,
+             2);
+
+  evalShould('add two numbers',
+             ['+', 2, 3], env,
+             5);
+
+  evalShould('multiply a number and variable',
+             ['*', 'y', 3], env,
+             9);
+
+  evalShould('combined variables',
+             ['/', 'z', ['+', 'x', 'y']], env,
+             2);
+
+});
 
 describe('quote', function() {
 
-    it('should quote a number', function() {
-      testEval(['quote', 3], {}, 
-               3);
-    });
+  evalShould('quote a number',
+             ['quote', 3], {}, 
+             3);
+  
+  evalShould('quote an atom',
+             ['quote', 'dog'], {}, 
+             'dog');
 
-    it('should quote an atom', function() {
-      testEval(['quote', 'dog'], {}, 
-               'dog');
-    });
-
-    it('should quote a list', function() {
-      testEval(['quote', [1, 2, 3]], {}, 
-               [1, 2, 3]);
-    });
+  evalShould('should quote a list',
+             ['quote', [1, 2, 3]], {},
+             [1, 2, 3]);
 
 });
 
