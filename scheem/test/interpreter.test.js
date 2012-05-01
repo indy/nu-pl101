32c28a82752441a55e43395692766f24de98837f
Inderjit Gill <email@indy.io>
2012-05-01 17:51:40 +0100

    interpreter tests parsed ast

--- scheem/test/interpreter.test.js
+++ scheem/test/interpreter.test.js
@@ -3,236 +3,409 @@ if (typeof module !== 'undefined') {
   // In Node load required modules
   var chai = require('chai');
   var interpreter = require('../lib/interpreter');
+  var peg = require('../lib/peg');
+  var parser = require('../lib/parser');
   var evalScheem = interpreter.evalScheem;
+  var evalScheemString = interpreter.evalScheemString;
 } else {
   // In browser assume already loaded by <script> tags
+  var peg = Scheem.peg;
+  var parser = Scheem.parser;
   var evalScheem = Scheem.interpreter.evalScheem;
+  var evalScheemString = Scheem.interpreter.evalScheemString;
 }
 
-function evalShould(desc, 
-                    expression, env, 
-                    expectedRes, expectedEnv) {
-  test(desc, function() {
-    chai.assert.deepEqual(evalScheem(expression, env), 
-                     expectedRes);
+// evalTest also compares the resultant AST, so we'll need
+// the parser
+var scheemParser = parser.buildParser(peg.pegDef);  
 
-    if(expectedEnv !== undefined) {
-      chai.assert.deepEqual(expectedEnv, env);
+function evalTest(str, env, expected) {
+  test(str, function() {
+    chai.assert.deepEqual(evalScheemString(str, env), 
+                          expected.res);
+    if(expected.env !== undefined) {
+      chai.assert.deepEqual(expected.env, env);
     }
 
+    chai.assert.deepEqual(expected.ast, scheemParser(str));
   });
 }
 
-suite('arithmetic', function() {
+function evalShouldThrow(desc, expression, env) {
+  test("should throw " + desc, function() {
+    chai.expect(function() {
+      evalScheem(expression, env);
+    }).to.throw();
+  });
+}
 
-  evalShould('return a number',
-             5, {},
-             5);
+suite('arithmetic: ', function() {
 
-  evalShould('add two numbers',
-             ['+', 2, 3], {},
-             5);
+  evalTest('5',
+           {},
+           {
+             ast:5,
+             env:{},
+             res:5
+           });
 
-  evalShould('multiply two numbers',
-             ['*', 2, 3], {},
-             6);
+  evalTest('(+ 2 3)',
+           {},
+           {
+             ast:['+', 2, 3],
+             env:{},
+             res:5
+           });
 
-  evalShould('subtract two numbers',
-             ['-', 5, 3], {},
-             2);
+  evalTest('(* 2 3)',
+           {},
+           {
+             ast:['*', 2, 3],
+             env:{},
+             res:6
+           });
 
-  evalShould('divide two numbers',
-             ['/', 10, 2], {},
-             5);
+  evalTest('(- 5 3)',
+           {},
+           {
+             ast:['-', 5, 3],
+             env:{},
+             res:2
+           });
 
-  evalShould('combined arithmetic operations',
-             ['*', ['/', 8, 4], ['+', 1, 1]], {},
-             4);
+  evalTest('(/ 10 2)',
+           {},
+           {
+             ast:['/', 10, 2],
+             env:{},
+             res:5
+           });
+
+  evalTest('(* (/ 8 4) (+ 1 1))',
+           {},
+           {
+             ast:['*', ['/', 8, 4], ['+', 1, 1]],
+             env:{},
+             res:4
+           });
 
 });
 
 
 
-suite('variables', function() {
+suite('variables: ', function() {
   
-  evalShould('return a number',
-             5, {x:2, y:3, z:10},
-             5);
+  evalTest('5',
+           {x:2, y:3, z:10},
+           {
+             ast:5,
+             env:{x:2, y:3, z:10},
+             res:5
+           });
 
-  evalShould('lookup a variable',
-             'x', {x:2, y:3, z:10},
-             2);
+  evalTest('x',
+           {x:2, y:3, z:10},
+           {
+             ast:'x',
+             env:{x:2, y:3, z:10},
+             res:2
+           });
 
-  evalShould('add two numbers',
-             ['+', 2, 3], {x:2, y:3, z:10},
-             5);
+  evalTest('(+ 2 3)',
+           {x:2, y:3, z:10},
+           {
+             ast:['+', 2, 3],
+             env:{x:2, y:3, z:10},
+             res:5
+           });
 
-  evalShould('multiply a number and variable',
-             ['*', 'y', 3], {x:2, y:3, z:10},
-             9);
+  evalTest('(* y 3)',
+           {x:2, y:3, z:10},
+           {
+             ast:['*', 'y', 3],
+             env:{x:2, y:3, z:10},
+             res:9
+           });
 
-  evalShould('combined variables',
-             ['/', 'z', ['+', 'x', 'y']], {x:2, y:3, z:10},
-             2);
-
-});
-
-suite('setting values', function() {
-
-  evalShould('define a value',
-             ['define', 'a', 5], {x:2, y:3, z:10},
-             0, {x:2, y:3, z:10, a:5});
-
-  evalShould('set! a value',
-             ['set!', 'z', 1], {x:2, y:3, z:10},
-             0, {x:2, y:3, z:1});
-
-  evalShould('set! a value',
-             ['set!', 'a', 1], {x:2, y:3, z:10},
-             0, {x:2, y:3, z:10, a:1});
-
-  evalShould('set! a value',
-             ['set!', 'y', ['+', 'x', 5]], {x:2, y:3, z:10},
-             0, {x:2, y:7, z:10});
-
-});
-
-suite('begin', function() {
-
-  evalShould('begin 1',
-             ['begin', 1, 2, 3], {},
-             3);
-
-  evalShould('begin 2',
-             ['begin', ['+', 2, 2]], {},
-             4);
-
-  evalShould('begin 3',
-             ['begin', 'x', 'y', 'x'], {x:1, y:2},
-             1);
-
-  evalShould('begin 3',
-             ['begin', 
-              ['set!', 'x', 5], 
-              ['set!', 'x', ['+', 'y', 'x']], 
-              'x'], {x:1, y:2},
-             7);
+  evalTest('(/ z (+ x y))',
+           {x:2, y:3, z:10},
+           {
+             ast:['/', 'z', ['+', 'x', 'y']],
+             env:{x:2, y:3, z:10},
+             res:2
+           });
 
 });
 
-suite('quote', function() {
+suite('setting values:', function() {
 
-  evalShould('quote a number',
-             ['quote', 3], {}, 
-             3);
+  evalTest('(define a 5)',
+           {x:2, y:3, z:10},
+           {
+             ast:['define', 'a', 5],
+             env:{x:2, y:3, z:10, a:5},
+             res:0
+           });
 
-  evalShould('quote an atom',
-             ['quote', 'dog'], {}, 
-             'dog');
+  evalTest('(set! z 1)',
+           {x:2, y:3, z:10},
+           {
+             ast:['set!', 'z', 1],
+             res:0,
+             env:{x:2, y:3, z:1}
+           });
 
-  evalShould('should quote a list',
-             ['quote', [1, 2, 3]], {},
-             [1, 2, 3]);
+  evalTest('(set! y (+ x 5))',
+             {x:2, y:3, z:10},
+             {
+               ast: ['set!', 'y', ['+', 'x', 5]], 
+               res: 0,
+               env: {x:2, y:7, z:10}
+             });
 
-  evalShould('should quote a list 2',
-             ['quote', ['+', 2, 3]], {},
-             ['+', 2, 3]);
+  evalShouldThrow("when set!'ing an undefined variable", 
+                  ['set!', 'y', 42], {});
 
-  evalShould('should quote a list 3',
-             ['quote', ['quote', ['+', 2, 3]]], {},
-             ['quote', ['+', 2, 3]]);
+});
 
-  evalShould('quote a number',
-             ["'", 3], {}, 
-             3);
+suite('begin: ', function() {
 
-  evalShould('quote an atom',
-             ["'", 'dog'], {}, 
-             'dog');
+  evalTest('(begin 1 2 3)',
+           {},
+           {
+             ast:['begin', 1, 2, 3],
+             env:{},
+             res:3
+           });
 
-  evalShould('should quote a list',
-             ["'", [1, 2, 3]], {},
-             [1, 2, 3]);
+  evalTest('(begin (+ 2 2))',
+           {},
+           {
+             ast:['begin', ['+', 2, 2]],
+             env:{},
+             res:4
+           });
 
-  evalShould('should quote a list 2',
-             ["'", ['+', 2, 3]], {},
-             ['+', 2, 3]);
+  evalTest('(begin x y x)',
+           {x:1, y:2},
+           {
+             ast:['begin', 'x', 'y', 'x'],
+             env:{x:1, y:2},
+             res:1
+           });
 
-  evalShould('should quote a list 3',
-             ["'", ["'", ['+', 2, 3]]], {},
-             ["'", ['+', 2, 3]]);
+  evalTest('(begin (set! x 5) (set! x (+ y x)) x)',
+           {x:1, y:2},
+           {
+             ast:['begin', 
+                  ['set!', 'x', 5], 
+                  ['set!', 'x', ['+', 'y', 'x']], 
+                  'x'],
+             env:{x:7, y:2},
+             res:7
+           });
+});
+
+suite('quote: ', function() {
+
+  evalTest('(quote 3)',
+           {},
+           {
+             ast:['quote', 3],
+             env:{}, 
+             res:3
+           });
+
+  evalTest('(quote dog)',
+           {},
+           {
+             ast:['quote', 'dog'],
+             env:{}, 
+             res:'dog'
+           });
+
+  evalTest('(quote (1 2 3))',
+           {},
+           {
+             ast:['quote', [1, 2, 3]],
+             env:{},
+             res:[1, 2, 3]
+           });
+
+  evalTest('(quote (+ 2 3))',
+           {},
+           {
+             ast:['quote', ['+', 2, 3]],
+             env:{},
+             res:['+', 2, 3]
+           });
+
+  evalTest('(quote (quote (+ 2 3)))',
+           {},
+           {
+             ast:['quote', ['quote', ['+', 2, 3]]],
+             env:{},
+             res:['quote', ['+', 2, 3]]
+           });
+
+  evalTest("'3",
+           {},
+           {
+             ast:['quote', 3], 
+             env:{}, 
+             res:3
+           });
+
+  evalTest("'dog",
+           {},
+           {
+             ast: ['quote', 'dog'],
+             env: {},
+             res: 'dog'
+           });
+
+  evalTest("'(1 2 3)",
+           {},
+           {
+             ast: ['quote', [1, 2, 3]],
+             env: {},
+             res: [1, 2, 3]
+           });
+
+  evalTest('(quote (+ 2 3))',
+           {},
+           {
+             ast:['quote', ['+', 2, 3]],
+             env: {},
+             res:['+', 2, 3]
+           });
+
+  evalTest('(quote (quote (+ 2 3)))',
+           {},
+           {
+             ast:['quote', ['quote', ['+', 2, 3]]],
+             env:{},
+             res:['quote', ['+', 2, 3]]
+           });
+});
 
 
+suite('working with values:', function() {
+
+  evalTest('(< 2 2)',
+           {},
+           {
+             ast:['<', 2, 2],
+             env:{}, 
+             res:'#f'
+           });
+
+  evalTest('(< 2 3)',
+           {},
+           {
+             ast:['<', 2, 3],
+             env:{}, 
+             res:'#t'
+           });
+  
+  evalTest('(< (+ 1 1) (+ 2 3))',
+           {},
+           {
+             ast:['<', ['+', 1, 1], ['+', 2, 3]],
+             env:{}, 
+             res:'#t'
+           });
   
 });
 
 
-suite('working with values', function() {
+suite('working with lists: ', function() {
 
-  evalShould('compare values 1',
-             ['<', 2, 2], {}, 
-             '#f');
+  evalTest('(quote (2 3))',
+           {},
+           {
+             ast:['quote', [2, 3]],
+             env:{}, 
+             res:[2, 3]
+           });
 
-  evalShould('compare values 2',
-             ['<', 2, 3], {}, 
-             '#t');
-  
-  evalShould('compare values 2',
-             ['<', ['+', 1, 1], ['+', 2, 3]], {}, 
-             '#t');
+  evalTest("(cons 1 '(2 3))",
+           {},
+           {
+             ast:['cons', 1, ['quote', [2, 3]]],
+             env:{}, 
+             res:[1, 2, 3]
+           });
+
+  evalTest("(cons '(1 2) '(3 4))",
+           {},
+           {
+             ast:['cons', 
+                  ['quote', [1, 2]], ['quote', [3, 4]]],
+             env:{}, 
+             res:[[1, 2], 3, 4]
+           });
+
+  evalTest("(car '((1 2) 3 4))",
+           {},
+           {
+             ast:['car', ['quote', [[1, 2], 3, 4]]],
+             env:{}, 
+             res:[1, 2]
+           });
+
+  evalTest("(cdr '((1 2) 3 4))",
+           {},
+           {
+             ast:['cdr', ['quote', [[1, 2], 3, 4]]],
+             env:{}, 
+             res:[3, 4]
+           });
   
 });
 
 
-suite('working with lists', function() {
+suite('conditionals: ', function() {
 
-  evalShould('(quote (2 3))',
-             ['quote', [2, 3]], {}, 
-             [2, 3]);
+  evalTest('(if (= 1 1) 2 3)',
+           {},
+           {
+             ast:['if', ['=', 1, 1], 2, 3],
+             env:{},
+             res:2
+           });
 
-  evalShould("(cons 1 '(2 3))",
-             ['cons', 1, ['quote', [2, 3]]], {}, 
-             [1, 2, 3]);
+  evalTest('(if (= 1 0) 2 3)',
+           {},
+           {
+             ast:['if', ['=', 1, 0], 2, 3],
+             env:{},
+             res:3
+           });
 
-  evalShould("(cons '(1 2) '(3 4))",
-             ['cons', 
-              ['quote', [1, 2]], ['quote', [3, 4]]], {}, 
-             [[1, 2], 3, 4]);
+  evalTest('(if (= 1 1) 2 error)',
+           {},
+           {
+             ast:['if', ['=', 1, 1], 2, 'error'],
+             env:{}, 
+             res:2
+           });
 
-  evalShould("(car '((1 2) 3 4))",
-             ['car', ['quote', [[1, 2], 3, 4]]], {}, 
-             [1, 2]);
+  evalTest('(if (= 1 0) error 3)',
+           {},
+           {
+             ast:['if', ['=', 1, 0], 'error', 3],
+             env:{},
+             res:3
+           });
 
-  evalShould("(cdr '((1 2) 3 4))",
-             ['cdr', ['quote', [[1, 2], 3, 4]]], {}, 
-             [3, 4]);
-  
-});
-
-
-suite('conditionals', function() {
-
-  evalShould('(if (= 1 1) 2 3) test',
-             ['if', ['=', 1, 1], 2, 3], {},
-             2);
-
-
-  evalShould('(if (= 1 0) 2 3) test',
-             ['if', ['=', 1, 0], 2, 3], {},
-             3);
-
-  evalShould('(if (= 1 1) 2 error) test',
-             ['if', ['=', 1, 1], 2, 'error'], {}, 
-             2);
-
-  evalShould('(if (= 1 1) error 3) test',
-             ['if', ['=', 1, 0], 'error', 3], {},
-             3);
-
-  evalShould('(if (= 1 1) (if (= 2 3) 10 11) 12) test',
-             ['if', ['=', 1, 1],
-              ['if', ['=', 2, 3], 10, 11], 12], {},
-             11);
-
+  evalTest('(if (= 1 1) (if (= 2 3) 10 11) 12)',
+           {},
+           {
+             ast:['if', ['=', 1, 1],
+                  ['if', ['=', 2, 3], 10, 11], 12],
+             env:{},
+             res:11
+           });
 });
 
