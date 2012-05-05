2bf9b6434cf7843e5ee2d10eb6d82acc11f3e0b0
Inderjit Gill <email@indy.io>
2012-05-05 15:21:51 +0100

    replaced switch statement with dispatch function

--- scheem/js/lib/interpreter.js
+++ scheem/js/lib/interpreter.js
@@ -12,19 +12,89 @@ if (typeof(Scheem) === 'undefined') {
 
 Scheem.interpreter = (function () {
 
-
   var eachArgument = function (expr, env, fn) {
     var i;
-    var res = evl(expr[1], env);
+    var res = eval(expr[1], env);
     for (i = 2;i < expr.length;i++) {
-      res = fn(res, evl(expr[i], env));
+      res = fn(res, eval(expr[i], env));
     }
     return res;
   };
 
-  var evl = function (expr, env) {
-    var i, xs, x, res, op;
+  var dispatchOn = {
+    '+': function(expr, env) {
+      return eachArgument(expr, env, function (a, b) { return a + b; });
+    },
+    '-': function(expr, env) {
+      return eachArgument(expr, env, function (a, b) { return a - b; });
+    },
+    '*': function(expr, env) {
+      return eachArgument(expr, env, function (a, b) { return a * b; });
+    },
+    '/': function(expr, env) {
+      return eachArgument(expr, env, function (a, b) { return a / b; });
+    },
+    'define': function(expr, env) {
+      env[expr[1]] = eval(expr[2], env);
+      return 0;
+    },
+    'set!': function(expr, env) {
+      if (env[expr[1]] === undefined) {
+        throw "trying to set! an undefined variable: " + expr[1];
+      } else {
+        env[expr[1]] = eval(expr[2], env);
+      }
+      return 0;
+    },
+    'begin': function(expr, env) {
+      var res;
+      for (var i = 1;i < expr.length;i += 1) {
+        res = eval(expr[i], env);
+      }
+      return res;
+    },
+    'quote': function(expr, env) {
+      if (expr.length !== 2) {
+        throw "need a single expression after 'quote'";
+      } 
+      return expr[1];
+    },
+    '=': function(expr, env) {
+      return (eval(expr[1], env) === eval(expr[2], env)) ? '#t' : '#f';
+    },
+    '<': function(expr, env) {
+      return (eval(expr[1], env) < eval(expr[2], env)) ? '#t' : '#f';
+    },
+    '>': function(expr, env) {
+      return (eval(expr[1], env) > eval(expr[2], env)) ? '#t' : '#f';
+    },
+    'cons': function(expr, env) {
+      var x = eval(expr[1], env);
+      var xs = eval(expr[2], env);
+      xs.unshift(x);
+      return xs;
+    },
+    'car': function(expr, env) {
+      var xs = eval(expr[1], env);
+      return xs[0];
+    },
+    'cdr': function(expr, env) {
+      var xs = eval(expr[1], env).slice();
+      xs.splice(0, 1);
+      return xs;
+    },
+    'if': function(expr, env) {
+      var res = eval(expr[1], env);
+      if (res === '#t') {
+        return eval(expr[2], env);
+      } else {
+        return eval(expr[3], env);
+      }
+    }
+  };
 
+
+  var eval = function (expr, env) {
     // Numbers evaluate to themselves
     if (typeof expr === 'number') {
       return expr;
@@ -38,78 +108,15 @@ Scheem.interpreter = (function () {
       return env[expr];
     }
 
-    // Look at head of list for operation
-    switch (expr[0]) {
-    case '+':
-      return eachArgument(expr, env, function (a, b) { return a + b; }); 
-    case '-':
-      return eachArgument(expr, env, function (a, b) { return a - b; });
-    case '*':
-      return eachArgument(expr, env, function (a, b) { return a * b; });
-    case '/':
-      return eachArgument(expr, env, function (a, b) { return a / b; });
-
-
-    case 'define':
-      env[expr[1]] = evl(expr[2], env);
-      return 0;
-    case 'set!':
-      if (env[expr[1]] === undefined) {
-        throw "trying to set! an undefined variable: " + expr[1];
-      } else {
-        env[expr[1]] = evl(expr[2], env);
-      }
-      return 0;
-    case 'begin':
-      for (i = 1;i < expr.length;i += 1) {
-        res = evl(expr[i], env);
-      }
-      return res;
-    case 'quote':
-      if (expr.length !== 2) {
-        throw "need a single expression after 'quote'";
-      } 
-      return expr[1];
-    case '=':
-      op = (evl(expr[1], env) === evl(expr[2], env));
-      return op ? '#t' : '#f';
-    case '<':
-      op = (evl(expr[1], env) < evl(expr[2], env));
-      return op ? '#t' : '#f';
-    case '>':
-      op = (evl(expr[1], env) > evl(expr[2], env));
-      return op ? '#t' : '#f';
-
-    case 'cons':
-      x = evl(expr[1], env);
-      xs = evl(expr[2], env);
-      xs.unshift(x);
-      return xs;
-    case 'car':
-      xs = evl(expr[1], env);
-      return xs[0];
-    case 'cdr':
-      xs = evl(expr[1], env).slice();
-      xs.splice(0, 1);
-      return xs;
-
-    case 'if':
-      res = evl(expr[1], env);
-      if (res === '#t') {
-        return evl(expr[2], env);
-      } else {
-        return evl(expr[3], env);
-      }
-
-    }
-
+    var op = expr[0];
+    return dispatchOn[op](expr, env);
   };
 
   return {
     evalScheem: function (expr, env) {
       var res;
-      expr.map(function (e) {
-        res = evl(e, env);
+      expr.forEach(function (e) {
+        res = eval(e, env);
       });
       return res;
     }
