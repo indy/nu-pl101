7d3f00532fc30936d903a89422611db9d83395e9
Inderjit Gill <email@indy.io>
2012-05-09 18:59:28 +0100

    lambda-one and lambda added

--- scheem/js/lib/interpreter.js
+++ scheem/js/lib/interpreter.js
@@ -12,42 +12,42 @@ if (typeof(Scheem) === 'undefined') {
 
 Scheem.interpreter = (function () {
 
-var newScope = function(env) {
-  return {bindings: {}, outer: env};
-};
+  var newScope = function(env) {
+    return {bindings: {}, outer: env};
+  };
 
-var addBinding = function (env, v, val) {
+  var addBinding = function (env, v, val) {
     env.bindings[v] = val;
     return env;
-};
-var lookup = function (env, v) {
+  };
+  var lookup = function (env, v) {
     if(env === {}) {
-        throw "unknown variable " + v;
+      throw "unknown variable " + v;
     }
     if(env.bindings[v] !== undefined) {
-        return env.bindings[v];
+      return env.bindings[v];
     }
     return lookup(env.outer, v);
-};
-var update = function (env, v, val) {
+  };
+  var mutate = function (env, v, val) {
     if(env === {}) {
-        throw "trying to set! an undefined variable: " + v;
+      throw "trying to set! an undefined variable: " + v;
     }
     if(env.bindings[v] !== undefined) {
-        env.bindings[v] = val;
-        return env;
+      env.bindings[v] = val;
+      return env;
     }
-    return update(env.outer, v, val);
-};
+    return mutate(env.outer, v, val);
+  };
 
 
   var reduceArgs = function(expr, env, fn) {
-    return expr.slice(2).reduce(function(a, b) { return fn(a, eval(b, env)); },
-                                eval(expr[1], env));
+    return expr.slice(2).reduce(function(a, b) { return fn(a, evl(b, env)); },
+                                evl(expr[1], env));
   };
 
   var bool = function(expr, env, fn) {
-    return fn(eval(expr[1], env), eval(expr[2], env)) ? '#t' : '#f';
+    return fn(evl(expr[1], env), evl(expr[2], env)) ? '#t' : '#f';
   };
 
   var dispatchOn = {
@@ -64,17 +64,17 @@ var update = function (env, v, val) {
       return reduceArgs(expr, env, function (a, b) { return a / b; });
     },
     'define': function(expr, env) {
-      env = addBinding(env, expr[1], eval(expr[2], env));
+      env = addBinding(env, expr[1], evl(expr[2], env));
       return 0;
     },
     'set!': function(expr, env) {
-      env = update(env, expr[1], eval(expr[2], env));
+      env = mutate(env, expr[1], evl(expr[2], env));
       return 0;
     },
     'begin': function(expr, env) {
       var res;
       expr.slice(1).forEach(function (e){
-        res = eval(e, env);
+        res = evl(e, env);
       });
       return res;
     },
@@ -85,15 +85,15 @@ var update = function (env, v, val) {
       return expr[1];
     },
     'cons': function(expr, env) {
-      var xs = eval(expr[2], env);
-      xs.unshift(eval(expr[1], env));
+      var xs = evl(expr[2], env);
+      xs.unshift(evl(expr[1], env));
       return xs;
     },
     'car': function(expr, env) {
-      return eval(expr[1], env)[0];
+      return evl(expr[1], env)[0];
     },
     'cdr': function(expr, env) {
-      return eval(expr[1], env).slice(1);
+      return evl(expr[1], env).slice(1);
     },
     '=': function(expr, env) {
       return bool(expr, env, function(a, b) { return a == b; });
@@ -105,28 +105,46 @@ var update = function (env, v, val) {
       return bool(expr, env, function (a, b) { return a > b; });
     },
     'if': function(expr, env) {
-      if (eval(expr[1], env) === '#t') {
-        return eval(expr[2], env);
+      if (evl(expr[1], env) === '#t') {
+        return evl(expr[2], env);
       } else {
-        return eval(expr[3], env);
+        return evl(expr[3], env);
       }
     },
     'let-one': function(expr, env) {
       var newEnv = addBinding(newScope(env), 
-                              expr[1], eval(expr[2], env));
-      return eval(expr[3], newEnv);
+                              expr[1], evl(expr[2], env));
+      return evl(expr[3], newEnv);
+    },
+    'lambda-one': function(expr, env) {
+      return function(_arg) {
+        return evl(expr[2], addBinding(newScope(env),
+                                        expr[1], _arg));
+      };
+    },
+    'lambda': function(expr, env) {
+      return function() {
+        var args = arguments;
+        var newEnv = newScope(env);
+
+        expr[1].map(function(e, i){
+          addBinding(newEnv, e, args[i]);
+        });
+
+        return evl(expr[2], newEnv);
+      };
     }
   };
 
 
   var argsAsArray = function(expr, env) {
     return expr.slice(1).reduce(function(a, b) { 
-                                  a.push(eval(b, env));
+                                  a.push(evl(b, env));
                                   return a;},
                                 []);
   };
 
-  var eval = function(expr, env) {
+  var evl = function(expr, env) {
     var op, fn, args;
 
     // Numbers evaluate to themselves
@@ -144,7 +162,7 @@ var update = function (env, v, val) {
       return dispatchOn[op](expr, env);
     } else {
       // function application
-      fn = eval(expr[0], env);
+      fn = evl(expr[0], env);
       args = argsAsArray(expr, env);
       return fn.apply(null, args);
     }
@@ -154,7 +172,7 @@ var update = function (env, v, val) {
     evalScheem: function(expr, env) {
       var res;
       expr.forEach(function (e) {
-        res = eval(e, env);
+        res = evl(e, env);
       });
       return res;
     }
--- scheem/js/test/functions.test.js
+++ scheem/js/test/functions.test.js
@@ -112,3 +112,114 @@ suite('function application ', function () {
               });
 
 });
+
+
+suite('lambda-one', function () {
+
+  test('defining a function', function () {
+    var env = {bindings:{},outer:{}};
+    var ast = scheemParser('(define plus-two (lambda-one x (+ x 2)))');
+    var expected = { ast: [['define',
+                            'plus-two',
+                            ['lambda-one', 'x', ['+', 'x', 2]]]],
+                res: 0
+              };
+
+    chai.assert.deepEqual(expected.ast, ast);
+    var res = evalScheem(ast, env);
+    chai.assert.deepEqual(res, expected.res);
+
+    // function calls in js
+    chai.assert.equal(env.bindings['plus-two'](33), 35);
+
+    // function calls in scheem
+    ast = scheemParser('(plus-two 77)');
+    res = evalScheem(ast, env);
+    chai.assert.equal(res, 79);
+
+  });
+
+  // calling an anonymous function
+  evalEnvTest('((lambda-one x (+ x 2)) 7)',
+              {bindings:{},outer:{}},
+              { ast: [[['lambda-one', 'x', ['+', 'x', 2]], 7]],
+                env: {bindings:{},outer:{}},
+                res: 9
+              });
+
+  test('passing a function as a value to another function', function() {
+    var env = {bindings:{},outer:{}};
+    var ast = scheemParser(['(define plus-two (lambda-one x (+ x 2)))',
+                            '(define pass-in-six (lambda-one f (f 6)))',
+                            '(pass-in-six plus-two)'].join(''));
+    var res = evalScheem(ast, env);
+    chai.assert.equal(res, 8);
+  });
+
+
+  test('argument to a function shadows a global variable', function() {
+    var env = {bindings:{},outer:{}};
+    var ast = scheemParser(['(define x 44)',
+                            '(define plus-two (lambda-one x (+ x 2)))',
+                            '(define y (plus-two 3))'].join(''));
+    var res = evalScheem(ast, env);
+    chai.assert.equal(env.bindings['x'], 44);
+    chai.assert.equal(env.bindings['y'], 5);
+  });
+
+  test('inner function uses values from enclosing function', function() {
+    var env = {bindings:{},outer:{}};
+    var ast = scheemParser(['(define foo (lambda-one y (lambda-one z (+ z y))))',
+                            '((foo 6) 7)'].join(''));
+    var res = evalScheem(ast, env);
+    chai.assert.equal(res, 13);
+  });
+
+});
+
+suite('lambda', function () {
+  test('lambda function', function() {
+    var env = {bindings:{},outer:{}};
+    var ast = scheemParser(['(define foo (lambda (x y) (+ x y)))',
+                            '(foo 6 7)'].join(''));
+    var res = evalScheem(ast, env);
+    chai.assert.equal(res, 13);
+  });
+
+
+  test('calling an anonymous function', function() {
+    var env = {bindings:{},outer:{}};
+    var ast = scheemParser('((lambda (x) (+ x 2)) 7)');
+    var res = evalScheem(ast, env);
+    chai.assert.equal(res, 9);
+  });
+
+  test('passing a function as a value to another function', function() {
+    var env = {bindings:{},outer:{}};
+    var ast = scheemParser(['(define plus-two (lambda (x) (+ x 2)))',
+                            '(define pass-in-six (lambda (f) (f 6)))',
+                            '(pass-in-six plus-two)'].join(''));
+    var res = evalScheem(ast, env);
+    chai.assert.equal(res, 8);
+  });
+
+
+  test('argument to a function shadows a global variable', function() {
+    var env = {bindings:{},outer:{}};
+    var ast = scheemParser(['(define x 44)',
+                            '(define plus-two (lambda (x) (+ x 2)))',
+                            '(define y (plus-two 3))'].join(''));
+    var res = evalScheem(ast, env);
+    chai.assert.equal(env.bindings['x'], 44);
+    chai.assert.equal(env.bindings['y'], 5);
+  });
+
+  test('inner function uses values from enclosing function', function() {
+    var env = {bindings:{},outer:{}};
+    var ast = scheemParser(['(define foo (lambda (y) (lambda (z) (+ z y))))',
+                            '((foo 6) 7)'].join(''));
+    var res = evalScheem(ast, env);
+    chai.assert.equal(res, 13);
+  });
+
+});
